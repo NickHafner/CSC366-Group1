c383766099de78a959ce8bc938318bf32ae6243c
Nicholas Hafner <nickhafner@outlook.com>
2019-05-08 12:57:58 -0500

    Ping Pong game finished

---
+++ PingPong/PingPong/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PingPong
+{
+    partial class gameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.paddlePictureBox = new System.Windows.Forms.PictureBox();
+            this.ballPictureBox = new System.Windows.Forms.PictureBox();
+            this.scoreTitleLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.gameOverLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.paddlePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Controls.Add(this.gameOverLabel);
+            this.gamePanel.Controls.Add(this.scoreLabel);
+            this.gamePanel.Controls.Add(this.scoreTitleLabel);
+            this.gamePanel.Controls.Add(this.ballPictureBox);
+            this.gamePanel.Controls.Add(this.paddlePictureBox);
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(800, 450);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // paddlePictureBox
+            // 
+            this.paddlePictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.paddlePictureBox.Location = new System.Drawing.Point(277, 403);
+            this.paddlePictureBox.Name = "paddlePictureBox";
+            this.paddlePictureBox.Size = new System.Drawing.Size(200, 20);
+            this.paddlePictureBox.TabIndex = 0;
+            this.paddlePictureBox.TabStop = false;
+            // 
+            // ballPictureBox
+            // 
+            this.ballPictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ballPictureBox.Location = new System.Drawing.Point(41, 294);
+            this.ballPictureBox.Name = "ballPictureBox";
+            this.ballPictureBox.Size = new System.Drawing.Size(30, 30);
+            this.ballPictureBox.TabIndex = 1;
+            this.ballPictureBox.TabStop = false;
+            // 
+            // scoreTitleLabel
+            // 
+            this.scoreTitleLabel.AutoSize = true;
+            this.scoreTitleLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTitleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.scoreTitleLabel.Location = new System.Drawing.Point(38, 31);
+            this.scoreTitleLabel.Name = "scoreTitleLabel";
+            this.scoreTitleLabel.Size = new System.Drawing.Size(72, 23);
+            this.scoreTitleLabel.TabIndex = 2;
+            this.scoreTitleLabel.Text = "Score: ";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.scoreLabel.Location = new System.Drawing.Point(125, 31);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(21, 23);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "0";
+            // 
+            // gameOverLabel
+            // 
+            this.gameOverLabel.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameOverLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gameOverLabel.Location = new System.Drawing.Point(352, 31);
+            this.gameOverLabel.Name = "gameOverLabel";
+            this.gameOverLabel.Size = new System.Drawing.Size(409, 261);
+            this.gameOverLabel.TabIndex = 4;
+            this.gameOverLabel.Text = "Game Over!\r\n\r\nF1 - Restart Game\r\n\r\nEsc - Exit\r\n";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // gameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "gameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameForm_KeyDown);
+            this.gamePanel.ResumeLayout(false);
+            this.gamePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.paddlePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label gameOverLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label scoreTitleLabel;
+        private System.Windows.Forms.PictureBox ballPictureBox;
+        private System.Windows.Forms.PictureBox paddlePictureBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PingPong/PingPong/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class gameForm : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int points = 0;
+
+
+        public gameForm()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            paddlePictureBox.Top = gamePanel.Bottom - (gamePanel.Bottom / 10);
+
+            gameOverLabel.Left = (gamePanel.Width/2) - (gameOverLabel.Width/2);
+            gameOverLabel.Top = (gamePanel.Height / 2) - (gameOverLabel.Height / 2);
+            gameOverLabel.Visible = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            paddlePictureBox.Left = Cursor.Position.X - (paddlePictureBox.Width / 2);
+
+            ballPictureBox.Left += speed_left;
+            ballPictureBox.Top += speed_top;
+
+            if (ballPictureBox.Bottom >= paddlePictureBox.Top && ballPictureBox.Bottom <= paddlePictureBox.Bottom && ballPictureBox.Left >= paddlePictureBox.Left && ballPictureBox.Right <= paddlePictureBox.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+                points += 1;
+                scoreLabel.Text = points.ToString();
+
+                Random r = new Random();
+                gamePanel.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+
+            if (ballPictureBox.Left <= gamePanel.Left)
+            {
+                speed_left = -speed_left;
+            }
+
+            if (ballPictureBox.Right >= gamePanel.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ballPictureBox.Top <= gamePanel.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ballPictureBox.Bottom >= gamePanel.Bottom)
+            {
+                timer1.Enabled = false;
+                gameOverLabel.Visible = true;
+            }
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { Application.Exit(); }
+            if (e.KeyCode == Keys.F1)
+            {
+                ballPictureBox.Top = 50;
+                ballPictureBox.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                scoreLabel.Text = "0";
+                timer1.Enabled = true;
+                gameOverLabel.Visible = false;
+                gamePanel.BackColor = Color.FromArgb(41, 44, 51);
+            }
+        }
+    }
+}
